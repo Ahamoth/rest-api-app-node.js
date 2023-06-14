b9be2e5ccbd461057f11056add72870457b31b23
Ahamoth <beeminer.store@gmail.com>
2023-06-14 17:19:08 +0300

    hw06-email

--- controllers/users.js
+++ controllers/users.js
@@ -1,13 +1,14 @@
 const { User } = require("../models/user");
-const { HttpError, ctrlWrapper } = require("../helpers");
+const { HttpError, ctrlWrapper, sendEmail } = require("../helpers");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const gravatar = require("gravatar");
 const jimp = require("jimp");
 const path = require("path");
 const fs = require("fs/promises");
+const { nanoid } = require("nanoid");
 
-const { SECRET_KEY } = process.env;
+const { SECRET_KEY, PORT } = process.env;
 const avatarDir = path.join(__dirname, "../", "public", "avatars");
 
 const register = async (req, res) => {
@@ -18,6 +19,7 @@ const register = async (req, res) => {
   }
   const hashPassword = await bcrypt.hash(password, 10);
   const avatarURL = gravatar.url(email);
+  const verificationCode = nanoid();
 
   const result = await User.create({
     name,
@@ -25,12 +27,72 @@ const register = async (req, res) => {
     password: hashPassword,
     subscription,
     avatarURL,
+    verificationCode,
   });
+
+  const verifyEmail = {
+    to: email,
+    subject: "Verify email",
+    html: `
+    <h1>Hi!<br/>
+You receive this message because you are eager to join our simple application. <br/>
+Please confirm your email address.</h1><br/>
+    <a target="_blank" href="http://localhost:${PORT}/api/auth/verify/${verificationCode}">Click to verify email</a>
+    `,
+  };
+
+  await sendEmail(verifyEmail);
+
   res.json({
     email: result.email,
     name: result.name,
     subscription: result.subscription,
     avatarURL: result.avatarURL,
+  });
+};
+
+const verify = async (req, res) => {
+  const { verificationCode } = req.params;
+  const user = await User.findOne({ verificationCode });
+  if (!user) {
+    throw HttpError(404, "User not found");
+  }
+  await User.findByIdAndUpdate(user._id, {
+    verify: true,
+    verificationCode: "",
+  });
+
+  res.json({
+    message: "Verification success",
+  });
+};
+
+const resendVerifyEmail = async (req, res) => {
+  const { email } = req.body;
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw HttpError(404, "Email not found");
+  }
+
+  if (user.verify) {
+    throw HttpError(404, "Email has been already verified");
+  }
+
+  const verifyEmail = {
+    to: email,
+    subject: "Verify email",
+    html: `
+    <h1>Hi!<br/>
+You receive this message because you are eager to join our simple application. <br/>
+Please confirm your email address.</h1><br/>
+    <a target="_blank" href="http://localhost:${PORT}/api/auth/verify/${user.verificationCode}">Click to verify email</a>
+    `,
+  };
+
+  await sendEmail(verifyEmail);
+
+  res.json({
+    message: "Email was sent successfully",
   });
 };
 
@@ -40,6 +102,11 @@ const login = async (req, res) => {
   if (!user) {
     throw HttpError(401, "Email or password is invalid");
   }
+
+  if (!user.verify) {
+    throw HttpError(401, "Email not verify");
+  }
+
   const passwordCompare = await bcrypt.compare(password, user.password);
   if (!passwordCompare) {
     throw HttpError(401, "Email or password is invalid");
@@ -110,4 +177,6 @@ module.exports = {
   getCurrent: ctrlWrapper(getCurrent),
   logout: ctrlWrapper(logout),
   updateAvatar: ctrlWrapper(updateAvatar),
+  verify: ctrlWrapper(verify),
+  resendVerifyEmail: ctrlWrapper(resendVerifyEmail),
 };
--- models/user.js
+++ models/user.js
@@ -33,6 +33,14 @@ const userSchema = new Schema({
     type: String,
     required: true,
   },
+  verify: {
+    type: Boolean,
+    default: false,
+  },
+  verificationCode: {
+    type: String,
+    default: "",
+  },
 });
 
 userSchema.post("save", mongooseHandleError);
@@ -48,9 +56,14 @@ const loginSchema = Joi.object({
   password: Joi.string().min(4).required(),
 });
 
+const emailSchema = Joi.object({
+  email: Joi.string().pattern(emailRegexp).required(),
+});
+
 const schemas = {
   registerSchema,
   loginSchema,
+  emailSchema,
 };
 
 const User = model("user", userSchema);
